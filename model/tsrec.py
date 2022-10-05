37d1bacdb1a92b3989b9bf5b576fac8a00ca2593
zhanglingzi <47054314+lzz0007@users.noreply.github.com>
2022-10-05 15:50:21 +0800

    tsrec.py add ii emb to seq emb before trm

--- model/tsrec.py
+++ model/tsrec.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from recbole.model.sequential_recommender.sasrec import SASRec
+import os
 
 
 class PWLayer(nn.Module):
@@ -59,7 +60,28 @@ class MoEAdaptorLayer(nn.Module):
         return multiple_outputs.sum(dim=-2)
 
 
-class UniSRec(SASRec):
+def build_knn_neighbourhood(adj, topk):
+    knn_val, knn_ind = torch.topk(adj, topk, dim=-1)
+    weighted_adjacency_matrix = (torch.zeros_like(adj)).scatter_(-1, knn_ind, knn_val)
+    return weighted_adjacency_matrix
+
+
+def compute_normalized_laplacian(adj):
+    rowsum = torch.sum(adj, -1)
+    d_inv_sqrt = torch.pow(rowsum, -0.5)
+    d_inv_sqrt[torch.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = torch.diagflat(d_inv_sqrt)
+    L_norm = torch.mm(torch.mm(d_mat_inv_sqrt, adj), d_mat_inv_sqrt)
+    return L_norm
+
+
+def build_sim(context):
+    context_norm = context.div(torch.norm(context, p=2, dim=-1, keepdim=True))
+    sim = torch.mm(context_norm, context_norm.transpose(1, 0))
+    return sim
+
+
+class tSRec(SASRec):
     def __init__(self, config, dataset):
         super().__init__(config, dataset)
 
@@ -67,16 +89,8 @@ class UniSRec(SASRec):
         self.temperature = config['temperature']
         self.lam = config['lambda']
 
-        assert self.train_stage in [
-            'pretrain', 'inductive_ft', 'transductive_ft'
-        ], f'Unknown train stage: [{self.train_stage}]'
-
-        if self.train_stage in ['pretrain', 'inductive_ft']:
-            self.item_embedding = None
-            # for `transductive_ft`, `item_embedding` is defined in SASRec base model
-        if self.train_stage in ['inductive_ft', 'transductive_ft']:
-            # `plm_embedding` in pre-train stage will be carried via dataloader
-            self.plm_embedding = copy.deepcopy(dataset.plm_embedding)
+        self.item_embedding = None
+        self.plm_embedding = copy.deepcopy(dataset.plm_embedding)
 
         self.moe_adaptor = MoEAdaptorLayer(
             config['n_exps'],
@@ -84,89 +98,62 @@ class UniSRec(SASRec):
             config['adaptor_dropout_prob']
         )
 
+        topk = 10
+        if os.path.exists('%s/text_adj_%d.pt'%(config['data_path'], topk)):
+            text_adj = torch.load('%s/text_adj_%d.pt'%(config['data_path'], topk))
+        else:
+            text_adj = build_sim(self.plm_embedding.weight.detach()[1:]) # pos 0 is pad emb
+            text_adj = build_knn_neighbourhood(text_adj, topk=topk)
+            text_adj = compute_normalized_laplacian(text_adj)
+            torch.save(text_adj, '%s/text_adj_%d.pt'%(config['data_path'], topk))
+        self.text_adj = text_adj.to(config['device'])
+
+        self.gcn_layers = 2
+
     def forward(self, item_seq, item_emb, item_seq_len):
         position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
         position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
         position_embedding = self.position_embedding(position_ids)
 
         input_emb = item_emb + position_embedding
-        if self.train_stage == 'transductive_ft':
-            input_emb = input_emb + self.item_embedding(item_seq)
+        # if self.train_stage == 'transductive_ft':
+        #     input_emb = input_emb + self.item_embedding(item_seq)
         input_emb = self.LayerNorm(input_emb)
         input_emb = self.dropout(input_emb)
 
         extended_attention_mask = self.get_attention_mask(item_seq)
 
         trm_output = self.trm_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
-        output = trm_output[-1]
-        output = self.gather_indexes(output, item_seq_len - 1)
+        output = trm_output[-1] # last trm layer output
+        # output = self.gather_indexes(output, item_seq_len - 1) # retrieve emb at the last pos
         return output  # [B H]
 
-    def seq_item_contrastive_task(self, seq_output, same_pos_id, interaction):
-        pos_items_emb = self.moe_adaptor(interaction['pos_item_emb'])
-        pos_items_emb = F.normalize(pos_items_emb, dim=1)
-
-        pos_logits = (seq_output * pos_items_emb).sum(dim=1, keepdim=True) / self.temperature
-        pos_logits = torch.exp(pos_logits)
-
-        neg_logits = torch.matmul(seq_output, pos_items_emb.transpose(0, 1)) / self.temperature
-        neg_logits = torch.where(same_pos_id, torch.tensor([0], dtype=torch.float, device=same_pos_id.device), neg_logits)
-        neg_logits = torch.exp(neg_logits).sum(dim=1)
-
-        loss = -torch.log(pos_logits / neg_logits)
-        return loss.mean()
-
-    def seq_seq_contrastive_task(self, seq_output, same_pos_id, interaction):
-        item_seq_aug = interaction[self.ITEM_SEQ + '_aug']
-        item_seq_len_aug = interaction[self.ITEM_SEQ_LEN + '_aug']
-        item_emb_list_aug = self.moe_adaptor(interaction['item_emb_list_aug'])
-        seq_output_aug = self.forward(item_seq_aug, item_emb_list_aug, item_seq_len_aug)
-        seq_output_aug = F.normalize(seq_output_aug, dim=1)
-
-        pos_logits = (seq_output * seq_output_aug).sum(dim=1, keepdim=True) / self.temperature
-        pos_logits = torch.exp(pos_logits)
-
-        neg_logits = torch.matmul(seq_output, seq_output_aug.transpose(0, 1)) / self.temperature
-        neg_logits = torch.where(same_pos_id, torch.tensor([0], dtype=torch.float, device=same_pos_id.device), neg_logits)
-        neg_logits = torch.exp(neg_logits).sum(dim=1)
-
-        loss = -torch.log(pos_logits / neg_logits)
-        return loss.mean()
-
-    def pretrain(self, interaction):
-        item_seq = interaction[self.ITEM_SEQ]
-        item_seq_len = interaction[self.ITEM_SEQ_LEN]
-        item_emb_list = self.moe_adaptor(interaction['item_emb_list'])
-        seq_output = self.forward(item_seq, item_emb_list, item_seq_len)
-        seq_output = F.normalize(seq_output, dim=1)
-
-        # Remove sequences with the same next item
-        pos_id = interaction['item_id']
-        same_pos_id = (pos_id.unsqueeze(1) == pos_id.unsqueeze(0))
-        same_pos_id = torch.logical_xor(same_pos_id, torch.eye(pos_id.shape[0], dtype=torch.bool, device=pos_id.device))
-
-        loss_seq_item = self.seq_item_contrastive_task(seq_output, same_pos_id, interaction)
-        loss_seq_seq = self.seq_seq_contrastive_task(seq_output, same_pos_id, interaction)
-        loss = loss_seq_item + self.lam * loss_seq_seq
-        return loss
-
     def calculate_loss(self, interaction):
-        if self.train_stage == 'pretrain':
-            return self.pretrain(interaction)
-
         # Loss for fine-tuning
         item_seq = interaction[self.ITEM_SEQ]
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
         item_emb_list = self.moe_adaptor(self.plm_embedding(item_seq))
-        seq_output = self.forward(item_seq, item_emb_list, item_seq_len)
         test_item_emb = self.moe_adaptor(self.plm_embedding.weight)
-        if self.train_stage == 'transductive_ft':
-            test_item_emb = test_item_emb + self.item_embedding.weight
+        # if self.train_stage == 'transductive_ft':
+        #     test_item_emb = test_item_emb + self.item_embedding.weight
 
-        seq_output = F.normalize(seq_output, dim=1)
+        # GCN
+        h = test_item_emb[1:]
+        for i in range(self.gcn_layers):
+            h = torch.sparse.mm(self.text_adj, h)
+        self.h = torch.cat((test_item_emb[0].unsqueeze(0), h)) # concat with pad
+        ego_emb_seq = self.h[item_seq]
+
+        seq_output = self.forward(item_seq, item_emb_list+ego_emb_seq, item_seq_len)
+        seq_output = F.normalize(seq_output, dim=-1)
         test_item_emb = F.normalize(test_item_emb, dim=1)
 
-        logits = torch.matmul(seq_output, test_item_emb.transpose(0, 1)) / self.temperature
+
+        # combine GCN with trm emb
+        # combined_emb = ego_emb_seq + seq_output
+        output = self.gather_indexes(seq_output, item_seq_len - 1)  # retrieve emb at the last pos
+
+        logits = torch.matmul(output, test_item_emb.transpose(0, 1)) / self.temperature
         pos_items = interaction[self.POS_ITEM_ID]
         loss = self.loss_fct(logits, pos_items)
         return loss
@@ -175,13 +162,15 @@ class UniSRec(SASRec):
         item_seq = interaction[self.ITEM_SEQ]
         item_seq_len = interaction[self.ITEM_SEQ_LEN]
         item_emb_list = self.moe_adaptor(self.plm_embedding(item_seq))
-        seq_output = self.forward(item_seq, item_emb_list, item_seq_len)
         test_items_emb = self.moe_adaptor(self.plm_embedding.weight)
-        if self.train_stage == 'transductive_ft':
-            test_items_emb = test_items_emb + self.item_embedding.weight
+        # if self.train_stage == 'transductive_ft':
+        #     test_items_emb = test_items_emb + self.item_embedding.weight
 
+        seq_output = self.forward(item_seq, item_emb_list+self.h[item_seq], item_seq_len)
         seq_output = F.normalize(seq_output, dim=-1)
         test_items_emb = F.normalize(test_items_emb, dim=-1)
 
+        # combined_emb = self.h[item_seq] + seq_output
+        seq_output = self.gather_indexes(seq_output, item_seq_len - 1)  # retrieve emb at the last pos
         scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B n_items]
         return scores
